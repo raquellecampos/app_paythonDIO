c089d60634a38f64856498acc1a76b0debca2621
raquellecampos <raquellecampos12@gmail.com>
2021-08-07 23:55:17 -0400

    aula 4

---
+++ aula4.py
@@ -0,0 +1,13 @@
+a = int(input('digite um número: '))
+
+div = 0
+for x in range(1, a+1):
+    resto = a % x
+    print(a, resto)
+    if resto == 0:
+        div += 1
+
+if div == 2:
+    print('número {} é primo'.format(a))
+else:
+    print('número {} não é primo'.format(a))
